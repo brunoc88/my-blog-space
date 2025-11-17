2537c86aa30796d7807000247b8c3061c8b6cf4e
brunoc88 <brunocerutti88@gmail.com>
2025-11-17 14:12:39 -0300

    frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Routes, Router } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Routes>
+      <Router path="" elemente={ }></Router>
+    </Routes>
+  )
+}
+
+export default App
